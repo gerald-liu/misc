fabda377067152fafd5be0a41396844f4a19410d
root <root@gerald-liu.localdomain>
2018-02-13 02:17:48 +0800

    Update

--- test/test.cpp
+++ test/test.cpp
@@ -2,7 +2,9 @@
 
 using namespace std;
 
-int main(){
-    cout << "test" << endl;
+int main() {
+    char test[4] = {0};
+    for (int i=0; i<4; i++) cout << test[i] << '\t';
+    cout << endl;
     return 0;
 }
