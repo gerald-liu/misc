012a2bac3da686d8b1f7cd6604d9f1b63f7ebf28
gerald-liu <36258909+gerald-liu@users.noreply.github.com>
2018-03-24 02:08:28 +0800

    test

--- test/default_ctor.cpp
+++ test/default_ctor.cpp
@@ -7,19 +7,21 @@ private:
 	int frequency; char* str;
 	void set(int f, const char* s) { frequency = f; str = new char [strlen(s)+1]; strcpy(str, s); }
 public:
-	Word(const char* s, int k = 1) { set(k, s); cout << "\nImplicit const char* conversion\n"; }
-	Word(const Word& w) { set(w.frequency, w.str); cout << "\nCopy\n"; }
-	Word() {set(0, "d"); cout << "default\n";}
+	Word() { set(0, "d"); cout << "default constructor\n"; }
+	~Word() { delete[] str; cout << "destructor\n"; }
 	void print() const { cout << str << " : " << frequency << " ; " 
 		<< reinterpret_cast<void*>(str) << endl; } // Also prints the address of object’s str array
 };
 
 int main() {
-	//Word w0();	// function declaration with name "w0" and return type Word
-	Word w1{};	// initialize with default constructor
-	Word w2;	// initialize with default constructor
+	
+	//Word;			// Compilation Error: declaration does not declare anything
+	Word();			// initialization with default constructor, POOR GUY
+	Word w;			// initialization with default constructor
 
-    //w0.print(); // Compilation Error: request for member 'print' in 'w0', which is of non-class type 'Word ()'
+	Word w0();		// function declaration with name "w0" and return type Word
+	Word w1{};		// initialization with default constructor
+
+    //w0.print();	// Compilation Error: request for member 'print' in 'w0', which is of non-class type 'Word ()'
 	w1.print();
-	w2.print();
 }
