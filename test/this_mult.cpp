8138c22ceca2b77a4894ecb6cb0e0693fea6d66f
gerald-liu <36258909+gerald-liu@users.noreply.github.com>
2018-05-02 12:14:17 +0800

    .

--- test/this_mult.cpp
+++ test/this_mult.cpp
@@ -6,7 +6,7 @@ class A {
 public:
 	A* print_this() { return this; } // print the address stored in the pointer
 	//A* print_this_mult(int n) { return this * n; } // invalid operands to binary 'operator*'
-	A* print_this_mult(int n) { return reinterpret_cast<A*>(reinterpret_cast<int>(this) * n); }
+	A* print_this_mult(int n) { return reinterpret_cast<A*>(reinterpret_cast<long>(this) * n); }
 };
 
 int main() {
