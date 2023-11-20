188de71819b5d58ab8fcf438331e781dcc3d205d
Gerald Liu <36258909+gerald-liu@users.noreply.github.com>
2023-11-21 01:10:47 +0800

    Delete md_to_pdf.bat

--- md_to_pdf.bat
+++
@@ -1,8 +0,0 @@
-setlocal
-set "wd=D:\OneDrive - HKUST Connect\Learning\Learning_MFin\FINA5360\Assignments"
-cd %wd%
-for %%f in (*.md) do (
-    pandoc --pdf-engine=xelatex %%f -o %%~nf.pdf
-)
-pause
-endlocal
